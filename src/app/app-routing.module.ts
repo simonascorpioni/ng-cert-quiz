cffe77077f1befdf60127362e5f56366ba8dcc2b
Simona Scorpioni <Simona.Scorpioni@eng.it>
2023-07-29 15:48:31 +0200

    Added step1 routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,14 @@ import {RouterModule, Routes} from '@angular/router';
 import {QuizMakerComponent} from './components/quiz-maker/quiz-maker.component';
 import {AnswersComponent} from './components/answers/answers.component';
 import {QuizService} from './services/quiz.service';
+import { QuizMaker2Component } from './components/quiz-maker2/quiz-maker2.component';
 
 const routes: Routes = [{
   path: "result", component: AnswersComponent, resolve: {data: () => inject(QuizService).getLatestResults()}
 },{
-  path: "**", component: QuizMakerComponent
+  path: "quiz-step1", component: QuizMakerComponent
+},{
+  path: "**", component: QuizMaker2Component
 }];
 
 @NgModule({
